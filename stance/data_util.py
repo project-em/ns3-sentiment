0a2094cd48708f7a42aad7d234c2555248cf92ac
Scott Freeman <sfreeman2494@gmail.com>
2017-03-17 13:54:55 -0400

    Fixes

--- stance/data_util.py
+++ stance/data_util.py
@@ -16,44 +16,14 @@ def correct_words(words):
 				corrected_words.append(one_word_corrs[0])
 	return corrected_words
 
-def load_data():
-	datapattern = 'data/abortion/*.data'
-	data = glob.glob(datapattern)
+# Load data for dueling LSTMs
+def load_dueling_data(filename):
+	datafile = open(filename,'r')
 
-	metapattern = 'data/abortion/*.meta'
-	meta = glob.glob(metapattern)
-
-	#load labels
-	print "loading labels"
-	Y = []
-	for i in range(len(meta)):
-		metaFile = open(meta[i], 'r')
-		line = metaFile.readline()
-		line = metaFile.readline()
-		line = metaFile.readline()
-		line = line.split("Stance=")[1]
-		y = int(line.split("\n")[0])
-		if y == 1:
-			Y.append(y)
-		else:
-			Y.append(0)
-
-	split = len(Y) / 4
-
-	y_train = np.array(Y[:split])
-	y_test = np.array(Y[split:])
-
-	print "y_train is: ", y_train
-	print "y_test is: ", y_test
-
-	print "total number of sentences is: ", len(Y)
-
-	#load sentence data
 	print "loading sentences"
-	Xlines = []
-	for i in range(len(data)):
-		dataFile = open(data[i], 'r')
-		Xlines.append(dataFile.readline())
+	Xlines = dataFile.readlines()
+
+	split = len(Xlines / 4)
 
 	Xlines_train = Xlines[:split]
 	Xlines_test = Xlines[split:]
@@ -89,4 +59,88 @@ def load_data():
 	x_train = np.array(x_train_arr)
 	x_test = np.array(x_test_arr)
 
-	return word_index, (x_train, y_train), (x_test, y_test)
+
+def load_data():
+ 	datapattern = 'data/abortion/*.data'
+ 	data = glob.glob(datapattern)
+ 
+ 	metapattern = 'data/abortion/*.meta'
+ 	meta = glob.glob(metapattern)
+ 
+ 	#load labels
+ 	print "loading labels"
+ 	Y = []
+ 	for i in range(len(meta)):
+ 		metaFile = open(meta[i], 'r')
+ 		line = metaFile.readline()
+ 		line = metaFile.readline()
+ 		line = metaFile.readline()
+ 		line = line.split("Stance=")[1]
+ 		y = int(line.split("\n")[0])
+ 		if y == 1:
+ 			Y.append(y)
+ 		else:
+ 			Y.append(0)
+ 
+ 	split = len(Y) / 4
+ 
+ 	y_train = np.array(Y[:split])
+ 	y_test = np.array(Y[split:])
+ 
+ 	print "y_train is: ", y_train
+ 	print "y_test is: ", y_test
+ 
+ 	print "total number of sentences is: ", len(Y)
+ 
+ 	#load sentence data
+ 	print "loading sentences"
+ 	Xlines = []
+ 	for i in range(len(data)):
+ 		dataFile = open(data[i], 'r')
+ 		Xlines += dataFile.readlines()
+ 
+ 	Xlines_train = Xlines[:split]
+ 	Xlines_test = Xlines[split:]
+ 	print "test lines are: ", Xlines_test
+ 
+ 	all_words = dict()
+ 	word_index = 1
+ 
+ 	x_train_arr = []
+ 	for line in Xlines_train:
+ 		words = filter(lambda w: len(w) > 0, line.split(" "))
+ 		corrected_words = correct_words(words)
+ 		int_words = []
+ 		for word in corrected_words:
+ 			if word not in all_words:
+ 				all_words[word] = word_index
+ 				word_index += 1
+ 			int_words.append(all_words[word])
+ 		x_train_arr.append(int_words)
+ 
+ 	x_test_arr = []
+ 	for line in Xlines_test:
+ 		words = filter(lambda w: len(w) > 0, line.split(" "))
+ 		corrected_words = correct_words(words)
+ 		int_words = []
+ 		# only add words that were seen in the training data
+ 		# TODO: is this necessary?
+ 		for word in corrected_words:
+ 			if word in all_words:
+ 				int_words.append(all_words[word])
+ 		x_test_arr.append(int_words)
+ 
+  	x_train = np.array(x_train_arr)
+  	x_test = np.array(x_test_arr)
+  
+ 	return word_index, (x_train, y_train), (x_test, y_test)
+ 	pos_indices = np.nonzero(y_test==1)
+ 	x_test_pos = x_test[pos_indices]
+ 	y_test_pos = y_test[pos_indices]
+ 
+ 	neg_indices = np.nonzero(y_test!=1)
+ 	x_test_neg = x_test[neg_indices]
+ 	y_test_neg = y_test[neg_indices]
+ 
+ 	return word_index, (x_train, y_train), (x_test_pos, y_test_pos), (x_test_neg, y_test_neg)
+  
--- stance/dueling_lstms.py
+++ stance/dueling_lstms.py
@@ -9,16 +9,50 @@ from article_utils import read_data_file
 
 import numpy as np
 
+def train(X, Y):
+	# load the dataset
+	num_words, (X_train, y_train), (X_test, y_test) = load_data()
+	
+	# truncate and pad input sequences
+	max_opinion_length = 200
+	X_train = sequence.pad_sequences(X_train, maxlen=max_opinion_length)
+	X_test = sequence.pad_sequences(X_test, maxlen=max_opinion_length)
+	
+	# create the model
+	embedding_vector_length = 32
+	model = Sequential()
+	model.add(Embedding(num_words, embedding_vector_length, input_length=max_opinion_length))
+	model.add(LSTM(100))
+	model.add(Dense(1, activation='sigmoid'))
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	print(model.summary())
+	model.fit(X_train, y_train, nb_epoch=3, batch_size=64)
+
+	# Final evaluation of the model
+	print "testing model"
+	scores = model.evaluate(X_test, y_test, verbose=0)
+	predictions = model.predict_classes(X_test)
+	print "predictions are: ", predictions
+	print("Accuracy: %.2f%%" % (scores[1]*100))
+
 def main():
 	# Load neutral data, label as 0
-	sentences = read_data_file()
-	y = np.zeros(len(sentences))
+	neutral_data_file = open("neutral.data", "r")
+	neutral_sentences = read_data_file(neutral_data_file)
+	y = np.zeros(len(neutral_sentences))
 
 	# Load liberal data, label as 1
+	liberal_data_file = open("liberal.data", "r")
+	liberal_sentences = read_data_file(liberal_data_file)
+	y = np.zeros(len(liberal_sentences))
 
 	# Load conservative data, label as -1
+	conservative_data_file = open("liberal.data", "r")
+	conservative_sentences = read_data_file(conservative_data_file)
+	y = np.zeros(len(conservative_sentences))
 
 	# Train liberal vs. neutral
+
 
 	# Train conservative vs. neutral
 
