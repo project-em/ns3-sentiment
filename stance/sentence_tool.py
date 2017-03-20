ee20744dbce8e52851b4bf28fcb515e2bbcbb20b
Scott Freeman <sfreeman2494@gmail.com>
2017-03-20 12:00:26 -0400

    Adding some comments

--- stance/sentence_tool.py
+++ stance/sentence_tool.py
@@ -10,6 +10,7 @@ DATABASE = os.getenv("DATABASE")
 PORT = os.getenv("PORT")
 HOST = os.getenv("HOST")
 
+# Connection to the SQL table
 connection = psycopg2.connect(
     host = HOST,
     database = DATABASE,
@@ -18,6 +19,8 @@ connection = psycopg2.connect(
     password = PASSWORD
 )
 
+
+# Writes the sentences to liberal and conservative data files
 def write_to_files():
 	articles = fetch_articles()
 
@@ -26,10 +29,13 @@ def write_to_files():
 
 	for row in articles:
 		text = row[2]
-		articleId = row[0]
 		sourceId = row[8]
+
+        # Load NLTK sentence tokenizer
 		sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 		sentences = sent_detector.tokenize(text.decode('utf-8'))
+
+
 		for sentence in sentences:
 			# Right now sourceId 5 is the only conservative source
 			if int(sourceId) != 5:
@@ -38,32 +44,42 @@ def write_to_files():
 				conservative_file.write(sentence.encode('utf-8') + os.linesep)
 
 
+# Labels sentences with bias and stores them into the SQL table
 def label_sentences():
 	cur = connection.cursor()
 
-	# Load our Keras model here
+	# Load Keras model here
 	# model = load_model('my_model.h5')
 
 	articles = fetch_articles();
+
 	for row in articles:
 		text = row[2]
 		articleId = row[0]
-		sourceId = row[8]
+
+        # Load NLTK sentence tokenizer
 		sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 		sentences = sent_detector.tokenize(text.decode('utf-8'))
+
 		for sentence in sentences:
 			# Label sentence here using our model
 			score = 0.1
+
+            # Store the sentence in the SQL table
 			cur.execute("INSERT INTO sentence (" + r'"text", "bias", "createdAt", "updatedAt", "articleId"' + ") VALUES (%s, %s, NOW(), NOW(), %s)", (sentence, str(score), str(articleId)))
 
 	connection.commit()
 
+# Fetches the articles from the SQL table
 def fetch_articles():
 	cur = connection.cursor()
 
 	cur.execute("SELECT * FROM article;")
 	return cur.fetchall()
 
+
+# Run program with 'label' to label sentences and put them in the DB
+# Run program with 'write' to write sentences to conservative and liberal data files
 def main():
 	if (len(sys.argv) > 1):
 		if (sys.argv[1] == "label"):
