97002b9d2ea50ec17f8723ed5f786a205fcc9f44
Nova Fallen <nfallen@seas.upenn.edu>
2017-03-23 15:50:40 -0400

    validation data and data_prep runs on python3 instead of python2, evaluate_model does separate tests on liberal and conservative

--- stance/data_prep.py
+++ stance/data_prep.py
@@ -2,11 +2,37 @@ import glob
 import json
 import nltk
 import os
+from enum import Enum
+from typing import Tuple, List
 
-def fetch_articles():
-    datapattern = 'data/training/570*/*.json'
+# Filenames to write training data
+cons_train_file = "data/training/conservative.dat"
+lib_train_file = "data/training/liberal.dat"
+
+# Filenames to write testing data
+cons_test_file = "data/testing/conservative.dat"
+lib_test_file = "data/testing/liberal.dat"
+neutral_test_file = "data/testing/neutral.dat"
+
+# Filenames to write validation data
+cons_valid_file = "data/valid/conservative.dat"
+lib_valid_file = "data/valid/liberal.dat"
+
+# Conservative or liberal model types for loading data
+class DataPurpose(Enum):
+    training = 1
+    testing = 2
+    validation = 3
+
+def fetch_articles(data_purpose):
+    # type: (DataPurpose) -> List[Tuple[str,str]]
+    if (data_purpose == DataPurpose.training):
+        datapattern = 'data/raw/570*/*.json'
+    elif (data_purpose == DataPurpose.testing):
+        datapattern = 'data/raw/test*/*.json'
+    elif (data_purpose == DataPurpose.validation):
+        datapattern = 'data/raw/valid*/*.json'
     data = glob.glob(datapattern)
-    print len(data)
     articles = []
     for i in range(len(data)):
         dataFile = open(data[i], 'r')
@@ -17,16 +43,31 @@ def fetch_articles():
 
     return articles
 
+
+
 # Given the name of an article folder (conservative, liberal, or neutral) read the 
 # articles from that folder and split them into sentences in a file of that name.
 # Writes the sentences to liberal and conservative data files
-def write_to_files():
-    articles = fetch_articles()
+def write_to_files(data_purpose):
+    # type: (DataPurpose) -> ()
+    articles = fetch_articles(data_purpose)
 
-    conservative_file = open('data/conservative.dat', 'w+')
-    liberal_file = open('data/liberal.dat', 'w+')
+    if data_purpose == DataPurpose.training:
+        os.makedirs("training", exist_ok=True)
+        conservative_file = open(cons_train_file, 'w+')
+        liberal_file = open(lib_train_file, 'w+')
+    elif data_purpose == DataPurpose.testing:
+        os.makedirs("testing", exist_ok=True)
+        conservative_file = open(cons_test_file, 'w+')
+        liberal_file = open(lib_test_file, 'w+')
+    elif data_purpose == DataPurpose.validation:
+        os.makedirs("valid", exist_ok=True)
+        conservative_file = open(cons_valid_file,'w+')
+        liberal_file = open(lib_valid_file, 'w+')
+    else:
+        print("invalid data purpose, exiting")
+        return
 
-    print articles[0][1]
     for article in articles:
         text = article[1].replace('\n', ' ')
         text = text.replace('\t', ' ')
@@ -39,20 +80,21 @@ def write_to_files():
         for sentence in sentences:
             # Right now sourceId 5 is the only conservative source
             if source == "thehill.com" or source == "foxnews.com":
-                conservative_file.write(sentence.encode('utf-8').strip() + os.linesep)
+                conservative_file.write(sentence.strip() + os.linesep)
             else:
-                liberal_file.write(sentence.encode('utf-8').strip() + os.linesep)
+                liberal_file.write(sentence.strip() + os.linesep)
 
+#TODO: write neutral data
 
 # Given the name of an article folder (conservative, liberal, or neutral) read the
 # articles from that folder and split them into sentences in a file of that name.
 # Writes the sentences to liberal and conservative data files
-def create_combined_data():
-    articles = fetch_articles()
+def create_combined_data(data_purpose):
+    articles = fetch_articles(data_purpose)
 
+    # TODO: rename
     training_file = open('data/training.dat', 'w+')
 
-    print articles[0][1]
     for article in articles:
         text = article[1].replace('\n', ' ')
         text = text.replace('\t', ' ')
@@ -63,13 +105,15 @@ def create_combined_data():
         sentences = sent_detector.tokenize(text)
 
         for sentence in sentences:
-            # Right now sourceId 5 is the only conservative source
             if source == "thehill.com" or source == "foxnews.com":
                 training_file.write(sentence.encode('utf-8').strip() + "\t-1" + os.linesep)
             else:
                 training_file.write(sentence.encode('utf-8').strip() + "\t1" + os.linesep)
 
+def main():
+    write_to_files(DataPurpose.training)
+    write_to_files(DataPurpose.testing)
+    write_to_files(DataPurpose.validation)
 
 if __name__ == '__main__':
-    create_combined_data()
-    #TODO: add "conservative" and "liberal" articles
+    main()
--- stance/evalulate_model.py
+++ stance/evalulate_model.py
@@ -3,49 +3,104 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from dueling_lstms import reload_model
 from dueling_lstms import label_sentence
-from sentence_tool import fetch_sentences
 from dueling_lstms import SourceStance
+from collections import Counter
 
-# Load models
-cons_model = None
-cons_vocab = None
-lib_model = None
-lib_vocab = None
+def eval_labeled_data():
+    # Load training and testing splits
+    sentences_training = open('data/training.dat', 'r')
+    sentences_testing = open('data/testing.dat', 'r')
 
-# cons_model, cons_vocab = reload_model(SourceStance.conservative)
-# lib_model, lib_vocab = reload_model(SourceStance.liberal)
+    # Load models
+    cons_model, cons_vocab = reload_model(SourceStance.conservative)
+    lib_model, lib_vocab = reload_model(SourceStance.liberal)
 
-# Load training and testing splits
-sentences_training = open('data/training.dat', 'r')
-sentences_testing = open('data/testing.dat', 'r')
+    Y_train = []
+    train_predictions = []
 
-Y_train = []
-train_predictions = []
+    # Label each training sentence
+    for line in sentences_training.readlines():
+        split = line.split("\t")
+        Y_train.append(int(split[1]))
+        sentence = split[0]
+        label = label_sentence(cons_model, lib_model, cons_vocab, lib_vocab, sentence)
+        train_predictions.append(label)
 
+    Y_test = []
+    test_predictions = []
 
-# Label each training sentence
-for line in sentences_training.readlines():
-    split = line.split("\t")
-    Y_train.append(int(split[1]))
-    sentence = split[0]
-    label = label_sentence(cons_model, lib_model, cons_model, lib_vocab, sentence)
-    train_predictions.append(label)
+    # Label each testing sentence
+    for line in sentences_testing.readlines():
+        split = line.split("\t")
+        Y_test.append(int(split[1]))
+        sentence = split[0]
+        label = label_sentence(cons_model, lib_model, cons_model, lib_vocab, sentence)
+        test_predictions.append(label)
 
-X_test = []
-Y_test = []
-test_predictions = []
+    # Calculate the training and testing precision scores
+    training_precision = precision_score(Y_train, train_predictions)
+    testing_precision = precision_score(Y_test, test_predictions)
 
-# Label each testing sentence
-for line in sentences_testing.readlines():
-    split = line.split("\t")
-    Y_test.append(int(split[1]))
-    sentence = split[0]
-    label = label_sentence(cons_model, lib_model, cons_model, lib_vocab, sentence)
-    test_predictions.append(label)
+    print("Training precision: ", str(training_precision))
+    print("Testing precision: ", str(testing_precision))
 
-# Calculate the training and testing precision scores
-training_precision = precision_score(Y_train, train_predictions)
-testing_precision = precision_score(Y_test, test_predictions)
+def eval_predict_provenance():
+    # Filenames of testing data
+    cons_test_file = "data/testing/conservative.dat"
+    lib_test_file = "data/testing/liberal.dat"
 
-print "Training precision: " + str(training_precision)
-print "Testing precision: " + str(testing_precision)
+    # Load models
+    cons_model, cons_vocab = reload_model(SourceStance.conservative)
+    lib_model, lib_vocab = reload_model(SourceStance.liberal)
+
+    Y_cons_test = []
+    cons_test_predictions = []
+    cons_preds_count = Counter()
+
+    # Label each testing sentence
+    print("labeling conservative sentences")
+    count = 0
+    for line in open(cons_test_file).readlines():
+        split = line.split("\t")
+        Y_cons_test.append(-1)
+        sentence = split[0]
+        label = label_sentence(cons_model, lib_model, cons_vocab, lib_vocab, sentence)
+        cons_preds_count[label] += 1
+        cons_test_predictions.append(label)
+        count += 1
+        if count % 100 == 0:
+            print("num sentences labeled is ", count)
+
+    # Calculate the training and testing precision scores
+    cons_testing_precision = precision_score(Y_cons_test, cons_test_predictions, average='micro')
+
+    Y_lib_test = []
+    lib_test_predictions = []
+    lib_preds_count = Counter()
+
+    # Label each testing sentence
+    print("labeling liberal sentences")
+    count = 0
+    for line in open(lib_test_file).readlines():
+        split = line.split("\t")
+        Y_lib_test.append(1)
+        sentence = split[0]
+        label = label_sentence(cons_model, lib_model, cons_vocab, lib_vocab, sentence)
+        lib_preds_count[label] += 1
+        lib_test_predictions.append(label)
+        count += 1
+        if count % 100 == 0:
+            print("num sentences labeled is ", count)
+
+    lib_testing_precision = precision_score(Y_lib_test, lib_test_predictions, average='micro')
+
+    print("Conservative testing precision: ", str(cons_testing_precision))
+    print("Conservative prediction counts: ", cons_preds_count)
+    print("Liberal testing precision: ", str(lib_testing_precision))
+    print("Liberal prediction counts: ", lib_preds_count)
+
+def main():
+    eval_predict_provenance()
+
+if __name__ == '__main__':
+    main()
